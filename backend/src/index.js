dfdb5d8dc6aa5108461f2b601e8d41b307e91f9f
Adamya Kumar <adamyakumar2307@gmail.com>
2025-02-25 15:47:09 +0530

    build complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,13 @@ import cookieParser from "cookie-parser";
 import { connectDB } from './lib/db.js';
 import { EventEmitter } from 'events';
 import cors from 'cors';
+import path from "path";
 import {server,app} from "./lib/socket.js"
 dotenv.config();
 EventEmitter.defaultMaxListeners = 20; 
 
 app.use(cookieParser());
-
+const __dirname = path.resolve(); 
 app.use(cors({
     origin:"http://localhost:5173",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -24,6 +25,14 @@ app.use("/api/messages",messageRouter);
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+if(process.nextTick.NODE_DEV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 server.listen(process.env.PORT, () => {
     console.log(`\x1b[34m\x1b[1mServer is running on http://localhost:${process.env.PORT}\x1b[0m`);
